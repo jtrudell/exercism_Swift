ba228eb5f0a94c9b9a9ca9c302b504052c8b9d74
Jen Trudell <jtrudell@gmail.com>
2016-01-16 17:26:49 -0600

    initial exercises

---
+++ hello-world/helloWorld.swift
@@ -0,0 +1,10 @@
+// Apple Swift version 2.0
+
+class HelloWorld {
+    class func hello() {
+        print("Hello, World!");
+    }
+    class func hello (name:String) {
+        print("Hello, \(name)!");
+    }
+}
